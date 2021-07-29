10624934202b8e8653b39e3fe237610c5ca2855b
prasantawstest <prasantawstest@gmail.com>
2021-07-29 16:49:49 -0500

    intial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
+  name1 : string;
   title = 'mealdemo';
+  constructor(){
+    this.name1 = "Hello";
+   
+  }
 }
